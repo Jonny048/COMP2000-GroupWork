220313bc452bdb007cbc5544e54394b59bb78ebc
David-Murphy0 <david.murphy3@students.mq.edu.au>
2025-08-04 10:04:00 +1000

    create names.java

---
+++ Names.java
@@ -0,0 +1,17 @@
+public class Names {
+	public static void main(String[] Args) {
+		System.out.println("This group consists of:");
+		// Put your signature function on the below line!
+		PrintDavidSig();
+		// Put your signature function on the above line!
+	}
+	
+	public static void PrintDavidSig() {
+		System.out.println(" ____              _     _   __  __                  _           \r\n" + //
+                        "|  _ \\  __ ___   _(_) __| | |  \\/  |_   _ _ __ _ __ | |__  _   _ \r\n" + //
+                        "| | | |/ _` \\ \\ / / |/ _` | | |\\/| | | | | '__| '_ \\| '_ \\| | | |\r\n" + //
+                        "| |_| | (_| |\\ V /| | (_| | | |  | | |_| | |  | |_) | | | | |_| |\r\n" + //
+                        "|____/ \\__,_| \\_/ |_|\\__,_| |_|  |_|\\__,_|_|  | .__/|_| |_|\\__, |\r\n" + //
+                        "                                              |_|          |___/ ");
+	}
+}
